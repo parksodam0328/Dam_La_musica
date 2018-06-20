75af1cf27063294389ff78f5fbef0a0e6fcf342e
Park So Dam <parksodam0228@e-mirim.hs.kr>
2018-06-20 21:16:40 +0900

    artist.html 작성

---
+++ test1.js
@@ -0,0 +1,43 @@
+$("#my-als-list").als({
+	visible_items: 4
+});
+$("#my-als-list").als({
+	scrolling_items: 2
+});
+$("#my-als-list").als({
+	orientation: "vertical"
+});
+$("#my-als-list").als({
+	circular: "yes"
+});
+$("#my-als-list").als({
+	autoscroll: "yes"
+});
+$("#my-als-list").als({
+	interval: 4000
+});
+$("#my-als-list").als({
+	direction: "down"
+});
+$("#my-als-list").als({
+	start_from: 2
+});
+$("#my-als-list").als({
+	speed: 400
+});
+$("#my-als-list").als({
+	easing: "linear"
+});
+
+$("#my-als-list").als({
+	visible_items: 4,
+	scrolling_items: 2,
+	orientation: "horizontal",
+	circular: "yes",
+	autoscroll: "yes",
+	interval: 6000,
+	speed: 400,
+	easing: "linear",
+	direction: "right",
+	start_from: 1
+});
